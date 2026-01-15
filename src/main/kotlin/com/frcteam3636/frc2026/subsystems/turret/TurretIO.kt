30df6a43cf1aaf0e4c8f29a1306407a3e2293803
ahollingsworth8064 <ahollingsworrth8064@student.pps.net>
2026-01-14 17:35:17 -0800

    finished encoder config and filled in some io functions

--- src/main/kotlin/com/frcteam3636/frc2026/subsystems/turret/TurretIO.kt
+++ src/main/kotlin/com/frcteam3636/frc2026/subsystems/turret/TurretIO.kt
@@ -1,14 +1,18 @@
 package com.frcteam3636.frc2026.subsystems.turret
 
 import com.ctre.phoenix6.BaseStatusSignal
+import com.ctre.phoenix6.configs.CANcoderConfiguration
 import com.ctre.phoenix6.configs.FeedbackConfigs
 import com.ctre.phoenix6.configs.TalonFXConfiguration
 import com.ctre.phoenix6.configs.TalonFXConfigurator
 import com.ctre.phoenix6.configs.TalonFXSConfiguration
+import com.ctre.phoenix6.controls.MotionMagicVoltage
 import com.ctre.phoenix6.controls.RainbowAnimation
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue
 import com.ctre.phoenix6.signals.InvertedValue
 import com.ctre.phoenix6.signals.NeutralModeValue
+import com.ctre.phoenix6.signals.SensorDirectionValue
+import com.frcteam3636.frc2026.CANcoder
 import com.frcteam3636.frc2026.CTREDeviceId
 import com.frcteam3636.frc2026.TalonFX
 import com.frcteam3636.frc2026.utils.math.PIDGains
@@ -19,8 +23,11 @@ import com.frcteam3636.frc2026.utils.math.radians
 import com.frcteam3636.frc2026.utils.math.rotationsPerSecond
 import com.frcteam3636.frc2026.utils.math.rotationsPerSecondPerSecond
 import edu.wpi.first.units.Units.Amps
+import edu.wpi.first.units.Units.Celsius
 import edu.wpi.first.units.Units.Radians
+import edu.wpi.first.units.Units.RadiansPerSecond
 import edu.wpi.first.units.measure.Angle
+import jdk.jfr.Enabled
 import org.team9432.annotation.Logged
 
 @Logged
@@ -29,17 +36,23 @@ open class TurretInputs{
     var turretAngle = Radians.zero()!!
     var turretCurrent = Amps.zero()!!
     var setPoint = Radians.zero()!!
+    var turretVelocity = RadiansPerSecond.zero()!!
+    var turretMotorTemperature = Celsius.zero()!!
+    var brakeMode = false
 }
 
 interface TurretIO{
     fun turnToAngle(angle: Angle)
     fun updateInputs(inputs: TurretInputs)
+    fun setBrakeMode(enabled: Boolean)
 
     val signals: Array<BaseStatusSignal>
         get() = emptyArray()
 }
 
 class TurretIOReal : TurretIO{
+    private var brakeMode = false
+
     private val turretTurningMotor = TalonFX(CTREDeviceId.TurretTurningMotor).apply {
         configurator.apply(TalonFXConfiguration().apply {
 
@@ -75,6 +88,7 @@ class TurretIOReal : TurretIO{
     private val currentSignal = turretTurningMotor.supplyCurrent
     private val temperatureSignal = turretTurningMotor.deviceTemp
 
+
     init{
         BaseStatusSignal.setUpdateFrequencyForAll(
             100.0,
@@ -83,15 +97,42 @@ class TurretIOReal : TurretIO{
             velocitySignal,
             temperatureSignal
         )
+        turretTurningMotor.optimizeBusUtilization()
+
+        CANcoder(CTREDeviceId.TurretTurningEncoder).apply {
+            configurator.apply(CANcoderConfiguration().apply {
+                MagnetSensor.MagnetOffset = MAGNET_OFFSET
+                MagnetSensor.SensorDirection = SensorDirectionValue.Clockwise_Positive
+            })
+        }
+    }
+    val positionControl: MotionMagicVoltage = MotionMagicVoltage(0.0).apply {
+        UpdateFreqHz = 0.0
     }
 
     override fun turnToAngle(angle: Angle) {
-        TODO("Not yet implemented")
+        turretTurningMotor.setControl(positionControl.withPosition(angle))
     }
 
     override fun updateInputs(inputs: TurretInputs) {
-        TODO("Not yet implemented")
+        inputs.turretAngle = positionSignal.value
+        inputs.turretCurrent = currentSignal.value
+        inputs.turretVelocity = velocitySignal.value
+        inputs.turretMotorTemperature = temperatureSignal.value
+        inputs.brakeMode = brakeMode
     }
+
+    override fun setBrakeMode(enabled: Boolean) {
+        brakeMode = enabled
+        turretTurningMotor.setNeutralMode(
+            if (enabled) {
+                NeutralModeValue.Brake
+            } else {
+                NeutralModeValue.Coast
+            }
+        )
+    }
+
 
     companion object Constants{
         private val PID_GAINS = PIDGains(62.0, 0.0, 0.0)
@@ -102,6 +143,7 @@ class TurretIOReal : TurretIO{
         private val PROFILE_JERK = 0.0
         private val PROFILE_VELOCITY = 12.0.rotationsPerSecond
     }
+
 }
 
     //kraken x44
