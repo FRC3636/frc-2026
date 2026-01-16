46761d64b72e1a8375713d0494f32de86356cf25
naojoy <njoy3663@student.pps.net>
2026-01-15 23:48:23 -0800

    turret alignment

--- src/main/kotlin/com/frcteam3636/frc2026/subsystems/turret/Turret.kt
+++ src/main/kotlin/com/frcteam3636/frc2026/subsystems/turret/Turret.kt
@@ -3,12 +3,18 @@ package com.frcteam3636.frc2026.subsystems.turret
 import com.frcteam3636.frc2026.Robot
 import com.frcteam3636.frc2026.subsystems.drivetrain.Drivetrain
 import com.frcteam3636.frc2026.subsystems.drivetrain.LimelightPoseProvider
-import com.frcteam3636.frc2026.utils.math.inRadians
-import com.frcteam3636.frc2026.utils.math.toRotation2d
+import com.frcteam3636.frc2026.utils.math.*
 import edu.wpi.first.math.filter.Debouncer
+import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.networktables.NetworkTableInstance
+import edu.wpi.first.units.measure.Distance
+import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj2.command.Subsystem
 import org.littletonrobotics.junction.Logger
+import java.sql.Driver
+import kotlin.jvm.optionals.getOrDefault
+import kotlin.jvm.optionals.getOrElse
+import kotlin.math.atan
 
 object Turret : Subsystem{
 
@@ -33,12 +39,26 @@ object Turret : Subsystem{
         Logger.processInputs("Turret", inputs)
     }
 
+    fun distanceToHub(): Distance {
+        val hubTranslation = DriverStation.getAlliance()
+            .orElse(DriverStation.Alliance.Blue)
+            .hubTranslation
+        return Drivetrain.estimatedPose.translation.getDistance(hubTranslation).meters
+    }
+
     fun aimAtHub() {
-        //TODO
-        val camError = turretLimelight.getEntry("tx")
-        var ty = 0.0
+        val camError = turretLimelight.getEntry("tx").getDouble(0.0)
+        val kP = -0.1
+        // align with limelight
         if (camError != null && inputs.seeTags) {
-            val tagDistance = turretLimelight.getEntry()
+            io.turnToAngle(inputs.turretAngle + (camError * kP).degrees)
+        } else {
+            // if no tags are seen then align with estimated pose
+            val hubTranslation = DriverStation.getAlliance()
+                .orElse(DriverStation.Alliance.Blue)
+                .hubTranslation
+            val turretAngle = atan((hubTranslation.y - Drivetrain.estimatedPose.y) / (hubTranslation.x - Drivetrain.estimatedPose.x))
+            io.turnToAngle(Math.toDegrees(turretAngle).degrees)
         }
     }
 
@@ -50,4 +70,16 @@ object Turret : Subsystem{
         io.setBrakeMode(false)
     }
 
+    val DriverStation.Alliance.hubTranslation
+        get() = when (this) {
+            DriverStation.Alliance.Blue -> Translation2d(
+                4.62534.meters,
+                (8.07 / 2).meters
+            )
+
+            else -> Translation2d(
+                (16.54 - 4.62534).meters,
+                (8.07 / 2).meters
+            )
+        }
 }
